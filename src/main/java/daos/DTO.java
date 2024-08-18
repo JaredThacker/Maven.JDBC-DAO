9d0b158d07bc7f156d032361a670dfaac2a8f5f6
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-08-18 12:38:14 -0400

    workinonit

---
+++ src/main/java/daos/DTO.java
@@ -0,0 +1,24 @@
+package daos;
+
+import models.Car;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DTO {
+
+    private Car extractUserFromResultSet(ResultSet rs) throws SQLException {
+        Car car = new Car();
+
+        car.setId( rs.getLong("id") );
+        car.setColor( rs.getString("color"));
+        car.setMake(rs.getString("make"));
+        car.setModel(rs.getString("model"));
+        car.setYear(rs.getInt("year"));
+        car.setVin(rs.getLong("vin"));
+
+        return car;
+    }
+
+
+}
